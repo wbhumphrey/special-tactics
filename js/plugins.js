610bd1a3a661ce73762398e5116d14e6c11a4a51
Brad Humphrey <brad@instructure.com>
2017-06-09 07:57:52 -0600

    add RandomMovement.js and basic map to test

--- js/plugins.js
+++ js/plugins.js
@@ -3,5 +3,5 @@
 var $plugins =
 [
 {"name":"TitleCommandPosition","status":true,"description":"Changes the position of the title command window.","parameters":{"Offset X":"200","Offset Y":"040","Width":"240","Background":"0"}},
-{"name":"ActionMenu","status":true,"description":"xxxxx   // Describe your plugin","parameters":{"xxxxx      //name of a parameter you want the user to edit":"zzzzz    // set default value for the parameter"}}
+{"name":"RandomMovement","status":true,"description":"Provides realisic bounded random movement for events","parameters":{"xxxxx      //name of a parameter you want the user to edit":"zzzzz    // set default value for the parameter"}}
 ];
--- js/plugins/RandomMovement.js
+++ js/plugins/RandomMovement.js
@@ -1,39 +1,61 @@
 //=============================================================================
 // Random Movement
-// by darien 
-// Date: 06/08/2017  
+// by darien
+// Date: 06/08/2017
 
 //=============================================================================
- 
+
 
 /*:
- * @plugindesc xxxxx   // Describe your plugin
- * @author yyyy        // your name goes here
+ * @plugindesc Provides realisic bounded random movement for events
+ * @author darien        // your name goes here
  *
  * @param xxxxx      //name of a parameter you want the user to edit
  * @desc yyyyy       //short description of the parameter
  * @default zzzzz    // set default value for the parameter
 
  */
-       
-       
+
+
  // Declare your function
 
- 
+
 // 1.  change *** to your plug ins file name below.
 // You are telling RPG maker that this plugin exsists.
-  (function() {
-  var parameters = PluginManager.parameters('Random Movement');
-  
+(function() {
+
+  var parameters = PluginManager.parameters('RandomMovement');
+
+  var _Game_Interpreter_pluginCommand = Game_Interpreter.prototype.pluginCommand;
+  Game_Interpreter.prototype.pluginCommand = function(command, args) {
+    _Game_Interpreter_pluginCommand.call(this, command, args);
+    if (command === 'RandomMovement') {
+      var gc = Game_Character;
+      character = this.character();
+      moveRoute = {
+        list:[
+          {code: gc.ROUTE_TURN_RANDOM, indent: null},
+          // {code: gc.ROUTE_MOVE_FORWARD, indent: null},
+          {}
+        ],
+        repeat: true,
+        skippable: true,
+        wait: true
+      };
+      character.forceMoveRoute(moveRoute);
+    }
+  };
+
+
   // NOTE: THIS WILL NOT MAKE YOU A DARK WIZARD. HERE ARE SOME BASE FOR YOU TO LAY A FOUNDATION FOR PLUGINS!
 // Now find something you want to edit in the core plugins.  You can
-// find them in the Project\www\js folder 
+// find them in the Project\www\js folder
 // 2. find the EXACT function you want to edit
-  
-  
+
+
   /*
   This function can be found in rpg_scenes.js
-  
+
   Scene_Title.prototype.drawGameTitle = function() {
     var x = 20;
     var y = Graphics.height / 4;
@@ -43,30 +65,30 @@
     this._gameTitleSprite.bitmap.outlineWidth = 8;
     this._gameTitleSprite.bitmap.fontSize = 72;
     this._gameTitleSprite.bitmap.drawText(text, x, y, maxWidth, 48, 'center');
-  
-  */
-  
 
-  
+  */
+
+
+
   // You need to come up with a name for your modification while keeping
   // class name the same.
-  
-  // the name of the function I want to edit is called 
-  //  Scene_Title.prototype.drawGameTitle 
+
+  // the name of the function I want to edit is called
+  //  Scene_Title.prototype.drawGameTitle
   // The name of my function will be called _Scene_Title_xxx
   //Follow for ease of use,  follow the template below.
   // Start your var as _NAME_NAME_YOURCLASSNAME
   // Have it equal the function you are replacing
-  
+
  //  var _Scene_Title_xxx = Scene_Title.prototype.drawGameTitle;
-  
+
  //  // make your adjustments by adding code, or adjusting them below.
  //  // This is an exact copy of the code above,  but with some of my adjustments
  //  // to some of the parameters.  Later,  I will show how you can call your parameters that the user adjusts
- //  // so your plugin has a little more control 
-  
- //    Scene_Title.prototype.drawGameTitle = function() { 
-  
+ //  // so your plugin has a little more control
+
+ //    Scene_Title.prototype.drawGameTitle = function() {
+
   // // _Scene_Title_xxx.call(this);    //sometimes you have to call your function to get this to work.  In this case you don't Ill explain why later.
 
  //    var x = 20;
@@ -77,11 +99,10 @@
  //    this._gameTitleSprite.bitmap.outlineWidth = 8;
  //    this._gameTitleSprite.bitmap.fontSize = 200;
  //    this._gameTitleSprite.bitmap.drawText(text, 0,0 , maxWidth, 48, 'center');
-  
-  
-  
-  
- //  } 
+
+
+
+
+ //  }
   })();  // dont touch this.
-  
-  
+
